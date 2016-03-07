8d122069bd3c2d62883155e04ca388e44a20c979
ernesto-pm <ernesto@itia.mx>
2016-03-07 14:09:27 -0600

    Now we can see different attributes in sfbay craigslist, by changing lines in size, next to 'class'

--- scraper.py
+++ scraper.py
@@ -1,7 +1,21 @@
 import requests
+from bs4 import BeautifulSoup as bs4
 
 url_base = 'http://sfbay.craigslist.org/search/eby/apa'
 params = dict(bedrooms=1 , is_furnished=1)
-rsp = requests.get(url_base,params=params)
 
-print(rsp.url)
+#Create Url with url_base and params
+rsp = requests.get(url_base,params=params)
+html = bs4(rsp.text,'html.parser')
+
+#print(rsp.url)
+#print(rsp.text[:500])
+#print(html.prettify()[:1000])
+apts = html.find_all('p', attrs={'class':'row'})
+#print(len(apts))
+
+this_appt = apts[98]
+#print(this_appt.prettify())
+
+size=this_appt.find_all(attrs={'class': 'price'})[0].text
+print(size)
